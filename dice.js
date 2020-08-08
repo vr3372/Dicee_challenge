014f0ba2180698461fea7b5049c0117bf7064442
Rajesh V R <48948666+vr3372@users.noreply.github.com>
2020-08-08 23:49:31 +0530

    javascript update

--- dice.js
+++ dice.js
@@ -16,31 +16,55 @@ function changePic() {
     nesImage.src = three;
     playerTwo.innerHTML = "player 2 wins";
     playerOne.innerHTML = "player 1";
+    playerTwo.style.color = "yellow";
+    playerOne.style.color = "rgb(250, 250, 219)";
+    playerOne.style.transform = "scale(1)";
+    playerTwo.style.transform = "scale(1.4)";
   } else if (rannum == 2) {
     curImage.src = four;
     nesImage.src = two;
     playerTwo.innerHTML = "player 2";
     playerOne.innerHTML = "player 1 wins";
+    playerOne.style.color = "yellow";
+    playerTwo.style.color = "rgb(250, 250, 219)";
+    playerOne.style.transform = "scale(1.4)";
+    playerTwo.style.transform = "scale(1)";
   } else if (rannum == 3) {
     curImage.src = three;
     nesImage.src = five;
     playerTwo.innerHTML = "player 2 wins";
     playerOne.innerHTML = "player 1";
+    playerTwo.style.color = "yellow";
+    playerOne.style.color = "rgb(250, 250, 219)";
+    playerOne.style.transform = "scale(1)";
+    playerTwo.style.transform = "scale(1.4)";
   } else if (rannum == 4) {
     curImage.src = four;
     nesImage.src = two;
     playerOne.innerHTML = "player 1 wins";
     playerTwo.innerHTML = "player 2";
+    playerOne.style.color = "yellow";
+    playerTwo.style.color = "rgb(250, 250, 219)";
+    playerOne.style.transform = "scale(1.4)";
+    playerTwo.style.transform = "scale(1)";
   } else if (rannum == 5) {
     ccurImage.src = one;
     nesImage.src = three;
     playerOne.innerHTML = "player 1";
     playerTwo.innerHTML = "player 2 wins";
+    playerTwo.style.color = "yellow";
+    playerOne.style.color = "rgb(250, 250, 219)";
+    playerOne.style.transform = "scale(1)";
+    playerTwo.style.transform = "scale(1.4)";
   } else if (rannum == 6) {
     curImage.src = six;
     nesImage.src = three;
     playerOne.innerHTML = "player 1 wins";
     playerTwo.innerHTML = "player 2";
+    playerOne.style.color = "yellow";
+    playerTwo.style.color = "rgb(250, 250, 219)";
+    playerOne.style.transform = "scale(1.4)";
+    playerTwo.style.transform = "scale(1)";
   } else {
     return "no image";
   }
