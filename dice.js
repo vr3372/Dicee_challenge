d36a7fd87513c675dc050a179275cae2b66a5461
Rajesh V R <48948666+vr3372@users.noreply.github.com>
2020-08-10 15:21:41 +0530

    javascript update

--- dice.js
+++ dice.js
@@ -1,70 +1,31 @@
-let one = "images/dice1.png";
-let two = "images/dice2.png";
-let three = "images/dice3.png";
-let four = "images/dice4.png";
-let five = "images/dice5.png";
-let six = "images/dice6.png";
+let images = [
+  "",
+  "images/dice1.png",
+  "images/dice2.png",
+  "images/dice3.png",
+  "images/dice4.png",
+  "images/dice5.png",
+  "images/dice6.png",
+];
 
 function changePic() {
   let curImage = document.querySelector(".first");
   let nesImage = document.querySelector(".second");
-  let playerOne = document.querySelector(".playone");
-  let playerTwo = document.querySelector(".playtwo");
-  let rannum = Math.round(Math.random() * 6 + 0.5);
-  if (rannum == 1) {
-    curImage.src = one;
-    nesImage.src = three;
-    playerTwo.innerHTML = "player 2 wins";
-    playerOne.innerHTML = "player 1";
-    playerTwo.style.color = "yellow";
-    playerOne.style.color = "rgb(250, 250, 219)";
-    playerOne.style.transform = "scale(1)";
-    playerTwo.style.transform = "scale(1.4)";
-  } else if (rannum == 2) {
-    curImage.src = four;
-    nesImage.src = two;
-    playerTwo.innerHTML = "player 2";
-    playerOne.innerHTML = "player 1 wins";
-    playerOne.style.color = "yellow";
-    playerTwo.style.color = "rgb(250, 250, 219)";
-    playerOne.style.transform = "scale(1.4)";
-    playerTwo.style.transform = "scale(1)";
-  } else if (rannum == 3) {
-    curImage.src = three;
-    nesImage.src = five;
-    playerTwo.innerHTML = "player 2 wins";
-    playerOne.innerHTML = "player 1";
-    playerTwo.style.color = "yellow";
-    playerOne.style.color = "rgb(250, 250, 219)";
-    playerOne.style.transform = "scale(1)";
-    playerTwo.style.transform = "scale(1.4)";
-  } else if (rannum == 4) {
-    curImage.src = four;
-    nesImage.src = two;
-    playerOne.innerHTML = "player 1 wins";
-    playerTwo.innerHTML = "player 2";
-    playerOne.style.color = "yellow";
-    playerTwo.style.color = "rgb(250, 250, 219)";
-    playerOne.style.transform = "scale(1.4)";
-    playerTwo.style.transform = "scale(1)";
-  } else if (rannum == 5) {
-    ccurImage.src = one;
-    nesImage.src = three;
-    playerOne.innerHTML = "player 1";
-    playerTwo.innerHTML = "player 2 wins";
-    playerTwo.style.color = "yellow";
-    playerOne.style.color = "rgb(250, 250, 219)";
-    playerOne.style.transform = "scale(1)";
-    playerTwo.style.transform = "scale(1.4)";
-  } else if (rannum == 6) {
-    curImage.src = six;
-    nesImage.src = three;
-    playerOne.innerHTML = "player 1 wins";
-    playerTwo.innerHTML = "player 2";
-    playerOne.style.color = "yellow";
-    playerTwo.style.color = "rgb(250, 250, 219)";
-    playerOne.style.transform = "scale(1.4)";
-    playerTwo.style.transform = "scale(1)";
+  let winner = document.querySelector(".caseone");
+  let firstDice = Math.round(Math.random() * 6 + 0.5);
+  let secondDice = Math.round(Math.random() * 6 + 0.5);
+  if (firstDice == secondDice) {
+    winner.textContent = "Draw!";
+    curImage.src = images[firstDice];
+    nesImage.src = images[secondDice];
+  } else if (firstDice > secondDice) {
+    winner.textContent = "Player 1 wins!";
+    curImage.src = images[firstDice];
+    nesImage.src = images[secondDice];
+  } else if (secondDice > firstDice) {
+    winner.textContent = "Player 2 wins!";
+    curImage.src = images[firstDice];
+    nesImage.src = images[secondDice];
   } else {
     return "no image";
   }
